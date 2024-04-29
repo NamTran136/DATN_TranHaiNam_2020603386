6b2db9fa246460819f3cac0699797dbd8ce4a2e8
Nam Tran <namnam13130606@gmail.com>
2024-04-29 10:36:33 +0700

    fix error and update reading book page

--- client/src/pages/public/readingBook.tsx
+++ client/src/pages/public/readingBook.tsx
@@ -20,6 +20,7 @@ const readingBook = () => {
         const book: BookDto = response.data;
         setBook(book);
         setError(null);
+        console.log(`https://drive.google.com/file/d/${book?.code}/preview`);
       })
       .catch((err) => {
         setError("Không có sách nào có ID = " + bookId);
@@ -29,14 +30,34 @@ const readingBook = () => {
         setIsLoading(false);
       });
   };
+var iframe = document.getElementById("fullscreen-iframe");
+  // Hàm để chuyển đổi chế độ fullscreen của iframe
+  function toggleFullscreen() {
+    if (
+      !document.fullscreenElement
+    ) {
+      if (iframe?.requestFullscreen) {
+        iframe.requestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  }
+  
   return (
     <div className="reading-book-wrapper">
+      <button className="default-button mb-5" onClick={() => toggleFullscreen()}>
+        Chế độ toàn màn hình
+      </button>
       {isLoading && <span>Loading...</span>}
       {error && <span className="red">Có lỗi xảy ra trong quá trình tải</span>}
       {book && (
         <iframe
           src={`https://drive.google.com/file/d/${book.code}/preview`}
-          allow="autoplay"
+          sandbox="allow-same-origin allow-scripts"
+          id="fullscreen-iframe"
         ></iframe>
       )}
     </div>
